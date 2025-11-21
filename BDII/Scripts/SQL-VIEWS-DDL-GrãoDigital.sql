c374b0cea587024906bcf735dca78d478036fa25
Marcelly Arcanjo <92063255+arcmarcelly@users.noreply.github.com>
2025-11-20 23:31:01 -0300

    views

---
+++ BDII/Scripts/SQL-VIEWS-DDL-GrãoDigital.sql
@@ -0,0 +1,172 @@
+-- -------------------------------------------------------------------
+    -- DDL - VIEWS - 10 PRINCIPAIS RELATÓRIOS --
+-- -------------------------------------------------------------------
+
+- --------------------------------------------------------------------------------------------------------
+-- 1.  Quais lotes foram entregues, mas ainda não possuem um registro de teste laboratorial associado? --
+-- -------------------------------------------------------------------------------------------------------
+SELECT
+    L.idLote,
+    L.tipo_semente,
+    L.data_recebimento,
+    F.nome_fornecedor
+FROM
+    Lote L
+JOIN
+    Fornecedor F ON L.Fornecedor_idFornecedor = F.idFornecedor
+WHERE
+    L.idLote NOT IN (
+        SELECT DISTINCT
+            Lote_idLote
+        FROM
+            TesteLaboratorial
+    );
+
+-- ------------------------------------------------------------------
+-- 2. -- Relatório de lotes aprovados e reprovados por fornecedor --
+-- ------------------------------------------------------------------
+SELECT
+    F.nome_fornecedor,
+    (SELECT COUNT(*)
+     FROM TesteLaboratorial T
+     JOIN Lote L2 ON L2.idLote = T.Lote_idLote
+     WHERE T.resultado = 'Aprovado'
+       AND L2.Fornecedor_idFornecedor = F.idFornecedor) AS total_aprovados,
+
+    (SELECT COUNT(*)
+     FROM TesteLaboratorial T
+     JOIN Lote L2 ON L2.idLote = T.Lote_idLote
+     WHERE T.resultado = 'Reprovado'
+       AND L2.Fornecedor_idFornecedor = F.idFornecedor) AS total_reprovados
+
+FROM Fornecedor F;
+
+-- -------------------------------------------------------------------------------------------------
+-- 4. Relatório completo do fluxo da semente: licitação → fornecedor → lote → saca → agricultor --
+-- -------------------------------------------------------------------------------------------------
+SELECT 
+    LCT.numero_processo,
+    F.nome_fornecedor AS fornecedor,
+    L.tipo_semente,
+    S.qr_code,
+    AGR.nome AS agricultor
+FROM Licitacao LCT
+JOIN Fornecedor F ON F.Licitacao_idLicitacao = LCT.idLicitacao
+JOIN Lote L ON L.Fornecedor_idFornecedor = F.idFornecedor
+JOIN Saca S ON S.Lote_idLote = L.idLote
+LEFT JOIN Notificacao N ON N.Saca_idSaca = S.idSaca
+LEFT JOIN Agricultor AGR ON AGR.idAgricultor = N.Agricultor_idAgricultor;
+
+-- -----------------------------------------------------------------------------------------------------
+-- 5.  Consulta de agricultores que receberam notificação que a saca estava disponível para retirada --
+-- -----------------------------------------------------------------------------------------------------
+SELECT 
+    A.nome AS agricultor,
+    N.data_envio AS data_notificacao
+FROM Notificacao N
+JOIN Agricultor A ON A.idAgricultor = N.Agricultor_idAgricultor
+WHERE N.status = 'Disponível'
+ORDER BY N.data_envio DESC;
+
+
+-- ----------------------------------------------------------------------------------------------------------------------------------------------------------
+-- 6. Relatório que lista os lotes de acordo com nome fornecedor e município do CD para entender quem forneceu cada lote e para onde ele foi enviado. --
+-- ----------------------------------------------------------------------------------------------------------------------------------------------------------
+SELECT 
+    L.idLote,
+    L.tipo_semente,
+    L.qtd_kg,
+    F.nome_fornecedor AS fornecedor,
+    CDE.cidade AS municipio_cd
+FROM Lote L
+JOIN Fornecedor F ON L.Fornecedor_idFornecedor = F.idFornecedor
+JOIN CentroDistribuicao_Endereco CDE 
+     ON CDE.CentroDistribuicao_idCentroDistribuicao = L.CentroDistribuicao_idCentroDistribuicao;
+
+-- -------------------------------------------------------------------
+-- 7.   Relatório de volume de sementes recebidas por município. --
+-- -------------------------------------------------------------------
+SELECT 
+    CDE.cidade AS municipio,
+    SUM(L.qtd_kg) AS total_kg
+FROM Lote L
+JOIN CentroDistribuicao_Endereco CDE 
+    ON CDE.CentroDistribuicao_idCentroDistribuicao = L.CentroDistribuicao_idCentroDistribuicao
+GROUP BY municipio
+ORDER BY total_kg DESC;
+
+-- ----------------------------------------------------------------------------------------------------------------------
+-- 10. Relatório dos Escritórios Locais que receberam sacas que foram transportadas pelo veículo com a placa 'XYZ9B87' --
+-- -----------------------------------------------------------------------------------------------------------------------
+SELECT DISTINCT
+    EL.nome_escritorio AS Escritorio_Local,
+    MS.data_hora AS Data_e_Hora_Movimentacao,
+    T.placa_veiculo AS Placa_do_Veiculo,
+    U.nome AS Nome_Operador_Movimentacao
+FROM
+    mydb.EscritorioLocal EL
+JOIN
+    mydb.Saca S ON EL.Saca_idSaca = S.idSaca
+JOIN
+    mydb.MovimentacaoSaca MS ON S.idSaca = MS.Saca_idSaca
+JOIN
+    mydb.Transporte T ON MS.Transporte_idTransporte = T.idTransporte
+JOIN
+    mydb.Usuario U ON MS.Usuario_idUsuario = U.idUsuario
+WHERE
+    MS.idMovimentacaoSaca IN (
+        -- Subconsulta: Encontra os IDs das Movimentações de Transporte da placa 'XYZ9B87'
+        SELECT idMovimentacaoSaca
+        FROM mydb.MovimentacaoSaca MS2
+        JOIN mydb.Transporte T2 ON MS2.Transporte_idTransporte = T2.idTransporte
+        WHERE T2.placa_veiculo = 'XYZ9B87'
+    )
+ORDER BY
+    MS.data_hora;
+
+-- -----------------------------------------------------------------
+-- 17. Consulta com detalhes de lote e fornecedor --
+-- ------------------------------------------------------------------
+SELECT
+    L.idLote,
+    L.tipo_semente,
+    L.qtd_kg,
+    F.nome_fornecedor
+FROM
+    Lote L
+JOIN
+    Fornecedor F ON L.Fornecedor_idFornecedor = F.idFornecedor
+ORDER BY
+    L.data_recebimento DESC;
+    
+-- ----------------------------------------------------------------------------------
+-- 19. Consulta do CD que fornece apenas 'Sementes Alfa'--
+-- ----------------------------------------------------------------------------------
+SELECT DISTINCT
+    CD.nome_centroDistribuicao
+FROM
+    mydb.CentroDistribuicao CD
+JOIN
+    mydb.Lote L ON CD.idCentroDistribuicao = L.CentroDistribuicao_idCentroDistribuicao
+WHERE
+    L.Fornecedor_idFornecedor IN (
+        -- Subconsulta: Encontra o ID do Fornecedor 'Sementes Alfa Ltda.'
+        SELECT idFornecedor
+        FROM mydb.Fornecedor
+        WHERE nome_fornecedor = 'Sementes Alfa Ltda.'
+    )
+ORDER BY
+    CD.nome_centroDistribuicao;
+
+
+-- -------------------------------------------------------------------
+-- 20. Lotes com maior quantidade de testes realizados--
+-- -------------------------------------------------------------------
+SELECT 
+    L.idLote,
+    L.tipo_semente,
+    COUNT(T.idTesteLaboratorial) AS total_testes
+FROM Lote L
+JOIN TesteLaboratorial T ON T.Lote_idLote = L.idLote
+GROUP BY L.idLote
+ORDER BY total_testes DESC;
